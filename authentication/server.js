996997539177e3b8af47216488f1f91a20f77426
Tanvee Balhara <balharatanvee@gmail.com>
2020-10-13 21:19:15 +0530

    changed links to static files

--- authentication/server.js
+++ authentication/server.js
@@ -16,7 +16,7 @@ initializePassport(passport);
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.static("./static"));
+app.use(express.static("static"));
 
 app.use(
     session({
