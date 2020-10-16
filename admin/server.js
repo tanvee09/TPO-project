fe00d21dd1ecaf3a00fcecc47042762e915e4495
Tanvee Balhara <balharatanvee@gmail.com>
2020-10-16 10:47:21 +0530

    admin login and send mail  option added

--- admin/server.js
+++ admin/server.js
@@ -6,6 +6,12 @@ const flash = require("express-flash");
 const session = require("express-session");
 require("dotenv").config();
 const app = express();
+const nodemailer = require('nodemailer');
+
+const ROLES = {
+    admin: 'admin',
+    users: 'users'
+  }
 
 const PORT = process.env.PORT || 5000;
 
@@ -42,19 +48,20 @@ app.get("/users/login", checkAuthenticated, (req, res) => {
     res.render("login.ejs");
 });
 
-
+app.get("/preparationcorner", (req, res) => {
+    res.render("./interviewprep/index")
+});
 
 app.get("/student",  (req, res) => {
     res.render("studentpage.ejs");
 });
 
-
-app.get("/adminpage", (req, res) => {
+app.get("/adminpage", checkNotAuthenticated, (req, res) => {
     res.render("adminpage.ejs");
 });
 
 app.get("/admin/login", checkAuthenticated, (req, res) => {
-    res.render("login.ejs");
+    res.render("adminlogin.ejs");
 });
 
 app.get("/users/dashboard", checkNotAuthenticated, (req, res) => {
@@ -65,6 +72,15 @@ app.get("/users/logout", (req, res) => {
     req.logOut();
     res.render("landingpage", { message: "You have logged out successfully" });
 });
+
+app.get("/admin/logout", (req, res) => {
+    req.logOut();
+    res.render("landingpage", { message: "You have logged out successfully" });
+});
+
+app.get("/admin/mail", checkNotAuthenticated, (req, res) => {
+    res.render("mail");
+})
 
 app.post("/users/register", async (req, res) => {
     let { name, email, password, password2 } = req.body;
@@ -129,6 +145,75 @@ app.post("/users/register", async (req, res) => {
     }
 });
 
+
+app.post("/admin/mail", async (req, res) => {
+    let { subject, mailbody} = req.body;
+
+    let errors = [];
+
+    console.log({
+        subject, mailbody
+    });
+
+    if (!subject || !mailbody) {
+        errors.push({ message: "Please enter all fields" });
+    }
+
+    if (errors.length > 0) {
+        res.render("mail", { errors, subject, mailbody });
+    } else {
+        pool.query(
+            `SELECT email FROM users WHERE isadmin = FALSE`,
+            [],
+            (err, results) => {
+                if (err) {
+                    console.log(err);
+                }
+                if (results.rows.length == 0) {
+                    errors.push({ message: "No student registered" });
+                    return res.render("mail", { errors, subject, mailbody });
+                } else {
+                    console.log(results.rows);
+
+                    let emails = [];
+
+                    for (x of results.rows) {
+                        emails.push(x['email']);
+                    }
+
+                    console.log(emails.join(', '));
+
+                    var transporter = nodemailer.createTransport({
+                        service: 'gmail',
+                        auth: {
+                            user: 'tpodept@gmail.com',
+                            pass: 'tpodept@2020'
+                        }
+                    });
+
+                    var mailOptions = {
+                        from: 'tpodept@gmail.com',
+                        to: emails.join(', '), //Tanvee get the receiver's email ids here
+                        subject: subject,
+                        text: mailbody
+                    };
+
+                    transporter.sendMail(mailOptions, function(error, info){
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log('Email sent: ' + info.response);
+                        }
+                    });
+
+                    req.flash("success_msg", "Emails have been sent.");
+                    res.redirect("/admin/mail");
+                }
+            }
+        );
+    }
+});
+
 app.post("/users/login", passport.authenticate('users', {
     successRedirect: "/student",
     failureRedirect: "/users/login",
@@ -156,6 +241,55 @@ function checkNotAuthenticated(req, res, next) {
     }
     res.redirect("/users/login");
 }
+
+// function checkAuthenticatedAsAdmin(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         console.log("uwu");
+//         let email = req.user.email;
+//         console.log("owo");
+//         pool.query(
+//             `SELECT * FROM users WHERE email = $1 AND isadmin = TRUE`,
+//             [email],
+//             (err, results) => {
+//               if (err) {
+//                     throw err;
+//               }
+      
+//               if (results.rows.length > 0) {
+//                     console.log("isAdmin");
+//                     return res.redirect("/adminpage");
+//               } else {
+//                     return next();
+//               }
+//             }
+//         );
+//     }
+//     return next();
+// }
+
+// function checkNotAuthenticatedAsAdmin(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         pool.query(
+//             `SELECT * FROM users WHERE email = $1 AND isadmin = TRUE`,
+//             [req.user.email],
+//             (err, results) => {
+//               if (err) {
+//                 throw err;
+//               }
+//               if (results.rows.length > 0) {
+//                     console.log("isAdmin");
+//                     return res.render("/adminpage");
+//               } else {
+//                 req.logOut();
+//                 return res.redirect("/admin/login");
+//               }
+//               return;
+//             }
+//         );
+//     }
+//     req.logOut();
+//     res.redirect("/admin/login");
+// }
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
